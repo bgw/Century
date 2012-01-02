1a21f7cd9330bb01f5a1bed66bf312c1354fd335
Century Developer <centurydevteam@gmail.com>
2012-01-02 17:41:23 -0500

    Made sphinx html documentation sidebar collapsible.

--- doc/conf.py
+++ doc/conf.py
@@ -98,7 +98,8 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"codebgcolor":"#ffe5bd", "footerbgcolor":"#a65b00"}
+html_theme_options = {"collapsiblesidebar":"true", "codebgcolor":"#ffe5bd",
+                      "footerbgcolor":"#a65b00"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
