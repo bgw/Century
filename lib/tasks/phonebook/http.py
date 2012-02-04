4317bd8008c9f98816e9fcfd9d4b60f85ea456df
Century Developer <centurydevteam@gmail.com>
2012-02-03 23:44:26 -0500

    Updated lib.tasks.phonebook HTTP Backend Use New Backend APIs
    Shifted some stuff around, rewrote a bunch of things to use
    DataHints instead of callback functions. Things are (IMHO) much
    cleaner now.
    
    Some stuff should still be moved out of the HTTP backend to a
    generic LDAP data field processor; allowing some code to be shared
    between the HTTP backend and a potential pure-LDAP backend.
    
    Of course, this is all experimental and nothing has been tested
    (yet).

--- lib/tasks/phonebook/http.py
+++ lib/tasks/phonebook/http.py
@@ -5,6 +5,7 @@ from ...browser import parsers
 import lxml
 import datetime
 import re
+import collections
 import logging
 try: # py3k
     import urllib.parse as urlpar
@@ -14,9 +15,20 @@ except: # py2
 logger = logging.getLogger("tasks.phonebook.http")
 
 _person_url_re = re.compile(
-    r"https?://.*?\.ufl\.edu(\:\d+)?(/private)?/people/"
+    r"https?://.*?\.ufl\.edu(\:\d+)?(?P<priv>/private)?/people/"
     r"(?P<ident>[A-Za-z0-9]*)/?"
 )
+
+class HttpLdapDataHint(DataHint):
+    def __init__(self, url):
+        DataHint.__init__(self)
+        self.url = url
+    
+    def __hash__(self):
+        return hash(self.url)
+    
+    def __eq__(self, other):
+        return self.url
 
 class HttpBackend(PhonebookBackend):
     """Provides the http backend for the UF phonebook. Data is pulled from the
@@ -25,6 +37,10 @@ class HttpBackend(PhonebookBackend):
     different fashion). If the data cannot be found, ``None`` is given. All
     values are of either type ``str`` or ``NoneType``, unless otherwise
     specified.
+    
+    Some information on UF's LDAP fields `can be found here
+    <http://www.webadmin.ufl.edu/projects/phonebook/ldap-field-spec.html>`_ and
+    `here <http://open-systems.ufl.edu/content/uf-ldap-schema>`_.
     
     *Keyword arguments:*
     
@@ -53,7 +69,7 @@ class HttpBackend(PhonebookBackend):
         A code representing what department this person belongs to.
     ``employee_number``
         If this person is employed by the University of Florida, this is their
-        employee number.
+        employee number. This is the same as one's UFID Number.
     ``affiliation``
         Described `here <https://phonebook.ufl.edu/affiliations/>`_. As of
         writing, possible values include "faculty", "staff", "student", and
@@ -68,10 +84,22 @@ class HttpBackend(PhonebookBackend):
     ``birth_date``
         An instance of :py:class:`datetype.date` representing the person's date
         of birth.
+    ``raw_ldap``
+        A dictionary of the raw fields pulled from the person's LDAP entry.
     """
     
     def __init__(self, browser):
         PhonebookBackend.__init__(self, browser)
+        self.__fields = {"url", "url_ldap", "url_full", "title", "phone",
+                         "preferred_phone", "email", "gatorlink_email",
+                         "gatorlink", "department_number", "employee_number",
+                         "affiliation", "address", "office_address", "language",
+                         "birth_date"}
+    
+    def get_fields(self):
+        return self.__fields
+    
+    fields = property(get_fields)
     
     def get_search_results(self, query, username, password):
         if username is not None and password is not None:
@@ -87,11 +115,12 @@ class HttpBackend(PhonebookBackend):
         
         if "returned only one" in info:
             attributes = {}
-            identifier = self._add_url_attributes(
-                attributes, self.browser.current_url
-            )
-            self._add_ldap_attributes(attributes)
-            return [Person(identifier, attributes), ]
+            url = self.browser.current_url
+            identifier = _person_url_re.match(url)
+            data_hint = \
+                HttpLdapDataHint(self.browser.expand_relative_url("full/", url))
+            return [Person(**{key:[data_hint] for key in self.fields},
+                           identifier=identifier)]
         elif "did not match any members" in info:
             return []
         else:
@@ -106,7 +135,6 @@ class HttpBackend(PhonebookBackend):
             return self.get_search_results_from_list(lxml_source)
     
     
-    
     def get_search_results_from_list(self, lxml_source):
         table = lxml_source.xpath("//div[@id='content']//table")[0]
         headers = [i.text.lower().strip() for i in table.xpath("./thead//th")]
@@ -115,112 +143,109 @@ class HttpBackend(PhonebookBackend):
         results = []
         for row in body:
             d = dict(zip(headers, row.xpath("./td")))
-            attributes = {}
             
-            identifier = self._add_url_attributes(attributes,
-                                                  d["name"][0].get("href"))
+            # process the url
+            url = d["name"][0].get("href")
+            url_ldap = self.browser.expand_relative_url("full/", url)
+            url_match = _person_url_re.match(url)
+            identifier = url_match.group("ident")
+            private = url_match.group("priv") > 0
             
-            attributes["name"] = d["name"].text_content().strip()
-            attributes["title"] = d["title"].text_content().strip()
-            attributes["phone"] = d["phone"].text.strip()
-            attributes["preferred_phone"] = attributes["phone"]
-            attributes["email"] = d["email"].text_content().strip()
-            if "@ufl.edu" in attributes["email"]:
-                attributes["gatorlink"] = attributes["email"][:-len("@ufl.edu")]
-                attributes["gatorlink_email"] = attributes["email"]
-            for i in attributes:
-                if attributes[i] == "":
-                    attributes[i] = None
-            self._add_ldap_attributes(attributes)
-            results.append(Person(identifier, attributes))
+            data_hint = HttpLdapDataHint(url_ldap)
+            # initially fill in all fields with DataHints, marking them unknown
+            attributes = {key:[data_hint] for key in self.fields}
+            
+            # fill in the attributes related to the url
+            attributes.update(
+                url=url, url_ldap=url_ldap, url_full=url_ldap,
+                url_vcard=self.browser.expand_relative_url("vcard/", url)
+            )
+            
+            name = d["name"].text_content().strip()
+            title = d["title"].text_content().strip()
+            phone = d["phone"].text_content().strip()
+            if not phone:
+                phone = None
+            email = d["email"].text_content().strip()
+            if not email:
+                email = None
+            elif "@ufl.edu" in email:
+                gatorlink = email[:-len("@ufl.edu")]
+                gatorlink_email = attributes["email"]
+                attributes.update(gatorlink=gatorlink,
+                                  gatorlink_email=gatorlink_email)
+            attributes.update(name=name, title=title, phone=phone,
+                              preferred_phone=phone, email=email)
+            results.append(Person(**attributes, identifier=identifier))
         return results
     
-    
-    def _add_url_attributes(self, attributes, url):
-        """Adds attributes based on the "url" attribute, and then returns the
-        identifier."""
-        print(url)
-        attributes["url"] = url
-        attributes["url_ldap"] = self.browser.expand_relative_url("full/", url)
-        attributes["url_full"] = attributes["url_ldap"]
-        attributes["url_vcard"] = self.browser.expand_relative_url("vcard/",
-                                                                   url)
-        return _person_url_re.match(url).group("ident")
-    
-    
-    def _add_ldap_attributes(self, attributes):
-        def load_ldap():
-            lxml_source = self.browser.load_page(attributes["url_ldap"],
-                                                 parser=parsers.lxml_html)
-            element_list = lxml_source.xpath("//div[@id='ldap']/dl")[0]
-            keys = [i.text.strip() for i in element_list.xpath("./dt")]
-            values = [i.text.strip() for i in element_list.xpath("./dd")]
-            data = dict(zip(keys, values))
-            
-            # utility function
-            def auto_fill(attr_key, *ldap_keys, apply_func=lambda x: x):
-                """You can specify multiple ``ldap_keys``, because the ldap
-                database contains tons of extra fields with (often) redundant
-                information that are sometimes used, sometimes not."""
-                if attributes[attr_key] is None or \
-                   hasattr(attributes[attr_key], "__call__"):
-                    for i in ldap_keys:
-                        if i not in data:
-                            continue
-                        attributes[attr_key] = apply_func(data[i])
-                        return
-                    attributes[attr_key] = None
-            
-            def fix_address(string):
-                return "\n".join([i.strip() for i in string.split("$")])
-            
-            auto_fill("name", "displayName", "cn")
-            
-            auto_fill("affiliation", "eduPersonPrimaryAffiliation")
-            if hasattr(attributes["title"], "__call__"):
-                base_title = data["title"] if "title" in data else \
-                             attributes["affiliation"]
-                if "o" in data:
-                    attributes["title"] = "%s, %s" % (base_title, data["o"])
-                else:
-                    attributes["title"] = base_title
-            
-            auto_fill("email", "mail")
-            auto_fill("phone", "telephoneNumber", "homePhone")
-            attributes["preferred_phone"] = attributes["phone"]
-            
-            auto_fill("gatorlink", "uid")
-            auto_fill("gatorlink", "homeDirectory", lambda s: s[3:])
-            if hasattr(attributes["gatorlink_email"], "__call__"):
-                attributes["gatorlink_email"] = "%s@ufl.edu" % \
-                                                attributes["gatorlink"]
-            auto_fill("department_number", "departmentNumber", "uflEduPsDeptId")
-            auto_fill("employee_number", "employeeNumber")
-            auto_fill("address", "postalAddress", "homePostalAddress",
-                      "registeredAddress", "uflEduOfficeLocation", "street",
-                      apply_func=fix_address)
-            auto_fill("office_address", "officeAddress", apply_func=fix_address)
-            auto_fill("language", "preferredLanguage")
-            auto_fill(
-                "birth_date", "uflEduBirthDate",
-                apply_func=lambda s:
-                    datetime.date(*[int(i) for i in s.split("-")])
-            )
+    def process_datahint(self, hint):
+        lxml_source = self.browser.load_page(hint.url, parser=parsers.lxml_html)
         
-        def function_writer(value):
-            def get_value():
-                load_ldap()
-                return attributes[value]
-            return get_value
+        element_list = lxml_source.xpath("//div[@id='ldap']/dl")[0]
+        keys = [i.text.strip() for i in element_list.xpath("./dt")]
+        values = [i.text.strip() for i in element_list.xpath("./dd")]
+        data = collections.defaultdict(lambda: None)
+        base_data = dict(zip(keys, values))
+        data.update(base_data)
         
-        provided_attributes = (
-            "name", "title", "phone", "preferred_phone", "email",
-            "gatorlink_email", "gatorlink", "department_number",
-            "employee_number", "affiliation", "address", "office_address",
-            "language", "birth_date"
-        )
+        # Utility Functions:
+        def fix_address(s): # Replace the $s in addresses with newlines
+            return s.replace("$", "\n") if s is not None else None
+        def select(data, *keys): # given keys, select the first one with a value
+            for i in keys:
+                if data[i]:
+                    return data[i]
+            return None
         
-        # write and add all the callbacks
-        for i in provided_attributes:
-            if i not in attributes or attributes[i] is None:
-                attributes[i] = function_writer(i)
+        # process some more complicated fields first:
+        base_title = select("title", "eduPersonPrimaryAffiliation")
+        if "o" in data:
+            if base_title is not None:
+                title = "%s, %s" % (base_title, data["o"])
+            else:
+                title = data["o"]
+        else:
+            title = base_title
+        phone = select(data, "telephoneNumber", "homePhone")
+        email = data["mail"]
+        if email and "@ufl.edu" in email:
+            gatorlink = email[:-len("@ufl.edu")]
+            gatorlink_email = attributes["email"]
+        else:
+            gatorlink = data["uid"] if data["uid"] \
+                                    else data["homeDirectory"][3:] \
+                                        if data["homeDirectory"] else None
+            gatorlink_email = ("%s@ufl.edu" % gatorlink) if gatorlink else None
+        birth_date = \
+            datetime.date(*[
+                int(i) for i in data["uflEduBirthDate"].split("-")
+            ]) if data["uflEduBirthDate"] else None
+        
+        # plug in and return ALL THE THINGS
+        return {k:v for k, v in {
+            "raw_ldap":base_data,
+            "affiliation":data["eduPersonPrimaryAffiliation"],
+            "name":select(data, "displayName", "cn", "gecos"),
+            "phone":phone,
+            "preferred_phone":phone,
+            "email":email,
+            "gatorlink":gatorlink,
+            "gatorlink_email":gatorlink_email,
+            "department_number":
+                select(data, "departmentNumber", "uflEduPsDeptId"),
+            "employee_number":data["employeeNumber"],
+            "address":
+                fix_address(
+                    select(data, "postalAddress",
+                           "homePostalAddress", "registeredAddress",
+                           "uflEduOfficeLocation", "street"
+                    )
+                ),
+            "office_address":
+                fix_address(
+                    select(data, "officeAddress", "uflEduOfficeLocation")
+                ),
+            "language":data["language"],
+            "birth_date":birth_date
+        }.items() if v is not None}
